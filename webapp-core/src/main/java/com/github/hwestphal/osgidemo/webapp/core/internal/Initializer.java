adf57960c3d287791d5b650f5ee6502d7cc23019
Harald Westphal <h.westphal@gmx.de>
2012-08-23 13:57:33 +0200

    add restlet stuff

--- webapp-core/src/main/java/com/github/hwestphal/osgidemo/webapp/core/internal/Initializer.java
+++ webapp-core/src/main/java/com/github/hwestphal/osgidemo/webapp/core/internal/Initializer.java
@@ -1,26 +1,36 @@
 package com.github.hwestphal.osgidemo.webapp.core.internal;
 
-import org.apache.felix.scr.annotations.Component;
+import javax.servlet.ServletException;
+
 import org.apache.felix.scr.annotations.Reference;
 import org.osgi.service.http.HttpContext;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
+import org.restlet.Component;
+import org.restlet.ext.servlet.ServerServlet;
 
-@Component
+@org.apache.felix.scr.annotations.Component
 public class Initializer {
 
 	private static final String ROOT_ALIAS = "/demo";
+	private static final String REST_ALIAS = "/demo/rest";
 
 	@SuppressWarnings("unused")
 	@Reference
 	private HttpService httpService;
 
-	public void bindHttpService(HttpService httpService) throws NamespaceException {
+	public void bindHttpService(HttpService httpService) throws NamespaceException, ServletException {
 		HttpContext httpContext = httpService.createDefaultHttpContext();
 		httpService.registerResources(ROOT_ALIAS, "/", httpContext);
+		ServerServlet restletServlet = new ServerServlet();
+		httpService.registerServlet(REST_ALIAS, restletServlet, null, httpContext);
+
+		Component component = restletServlet.getComponent();
+		component.getDefaultHost().attach(REST_ALIAS + "/", TestResource.class);
 	}
 
 	public void unbindHttpService(HttpService httpService) {
+		httpService.unregister(REST_ALIAS);
 		httpService.unregister(ROOT_ALIAS);
 	}
 
