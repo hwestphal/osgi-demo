93155282a45f06ed98a78ae0c9905fdb5cfc1f98
Harald Westphal <h.westphal@gmx.de>
2012-08-27 15:19:07 +0200

    add client code

--- webapp-core/src/main/resources/demo.js
+++ webapp-core/src/main/resources/demo.js
@@ -1,11 +1,33 @@
 (function() {
 	"use strict";
 
-	$.getJSON('rest/', {}, function(data) {
-		console.log(data);
-	});
-
+	var reload = function() {
+		$.getJSON('rest/', {}, function(data) {
+			$(function() {
+				var ul = $('#sidebar > ul');
+				ul.empty();
+				$.each(data, function(index, value) {
+					ul.append('<li>' + value['label'] + '</li>');
+					var li = ul.find('li:last');
+					li.on('click', function() {
+						$.getJSON('rest/' + value['id'], {}, function(data) {
+							var content = $('#data');
+							content.empty();
+							content.append(JSON.stringify(data['content']));
+						});					
+					});
+				});
+			});
+		});
+	};
+	
+	reload();
+	
 	$(function() {
+		$('#reload').on('click', function() {
+			$('#data').empty();
+			reload();
+		});
 	});
-
+	
 })();
