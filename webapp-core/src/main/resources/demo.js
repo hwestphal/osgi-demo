234b7138bf09f6d9446ad6586b422a9c0b824624
Harald Westphal <h.westphal@gmx.de>
2012-08-30 18:01:03 +0200

    use google maps for visualization of addons

--- webapp-core/src/main/resources/demo.js
+++ webapp-core/src/main/resources/demo.js
@@ -1,19 +1,36 @@
 (function() {
 	"use strict";
 
+	var map;
+	var marker = null;
+
 	var reload = function() {
 		$.getJSON('rest/', {}, function(data) {
 			$(function() {
 				var ul = $('#sidebar > ul');
+				if (marker !== null) {
+					marker.setMap(null);
+					marker = null;
+				}
 				ul.empty();
 				$.each(data, function(index, value) {
 					ul.append('<li>' + value['label'] + '</li>');
 					var li = ul.find('li:last');
 					li.on('click', function() {
 						$.getJSON('rest/' + value['id'], {}, function(data) {
-							var content = $('#data');
-							content.empty();
-							content.append(JSON.stringify(data));
+							if (marker !== null) {
+								marker.setMap(null);
+							}
+							marker = new google.maps.Marker({
+								position : new google.maps.LatLng(data['lat'],
+										data['lng']),
+								map : map,
+								title : data['label']
+							});
+							var infoWindow = new google.maps.InfoWindow({
+								content : data['description']
+							});
+							infoWindow.open(map, marker);
 						});
 					});
 				});
@@ -24,6 +41,13 @@
 	reload();
 
 	$(function() {
+		var mapOptions = {
+			zoom : 2,
+			center : new google.maps.LatLng(10, -10),
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
 		$('#reload').on('click', function() {
 			$('#data').empty();
 			reload();
